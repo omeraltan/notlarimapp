22048b87a6a015c831f34b9886d315af8426904c
omeraltan <omeraltan66@gmail.com>
2022-12-18 01:38:25 +0300

    Initial version of secondproject generated by generator-jhipster@7.9.3

---
+++ secondproject/src/main/java/com/second/app/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.second.app.repository;
+
+import com.second.app.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
