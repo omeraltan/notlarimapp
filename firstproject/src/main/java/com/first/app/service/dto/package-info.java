773ac7cfc4e07694b32ff7083b2764d2817b1a7e
omeraltan <omeraltan66@gmail.com>
2022-12-18 01:23:08 +0300

    Initial version of firstproject generated by generator-jhipster@7.8.1

---
+++ firstproject/src/main/java/com/first/app/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.first.app.service.dto;
---
+++ firstproject/src/test/java/com/first/app/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.first.app;
+
+import com.first.app.FirstprojectApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = FirstprojectApp.class)
+public @interface IntegrationTest {
+}
